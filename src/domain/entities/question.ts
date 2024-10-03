12e76c5fa8a90ef953c3c89569e4ee8b5a560182
unknown <eriicsouzaromeroo@gmail.com>
2024-10-03 14:48:17 -0300

    Add: Entity Id

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,11 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps{
     title:string
     content:string
-    authorId:string
+    authorId:UniqueEntityId
     slug: Slug
 }
 
