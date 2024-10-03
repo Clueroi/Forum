78bc1c91397f32a78dbe7706154633caa775b731
unknown <eriicsouzaromeroo@gmail.com>
2024-10-03 14:56:45 -0300

    Chore: Mapping priorities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,10 +3,13 @@ import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps{
+    authorId:UniqueEntityId
+    bestAnswerId?:UniqueEntityId
     title:string
     content:string
-    authorId:UniqueEntityId
     slug: Slug
+    createdAt:Date
+    updatedAt?:Date
 }
 
 export class Question extends Entity<QuestionProps>{
