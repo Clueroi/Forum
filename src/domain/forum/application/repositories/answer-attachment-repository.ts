acfec4a514dd93a023eee9f249802e0603837c47
unknown <eriicsouzaromeroo@gmail.com>
2024-10-20 17:57:57 -0300

    Docs: Spacements

--- src/domain/forum/application/repositories/answer-attachment-repository.ts
+++ src/domain/forum/application/repositories/answer-attachment-repository.ts
@@ -2,6 +2,6 @@ import { AnswerAttachment } from "../../enterpriste/entities/answer-attachment";
 
 export interface AnswerAttachmentRepository {
     findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
-    deleteManyByAnswerId(answerId:string):Promise<void>
-}   
+    deleteManyByAnswerId(answerId: string): Promise<void>
+}
 
--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -4,33 +4,33 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-r
 import { InMemoryAnswerAttachmentRepository } from 'test/repositories/in-memory-answer-attachment-repository'
 
 let inMemoryRepository: InMemoryAnswersRepository
-let inMemoryAnswerAttachment:InMemoryAnswerAttachmentRepository
-let sut:AnswerQuestionUseCase
+let inMemoryAnswerAttachment: InMemoryAnswerAttachmentRepository
+let sut: AnswerQuestionUseCase
 
-describe('Create an answer',()=>{
-    
-    beforeEach(()=>{
-        inMemoryAnswerAttachment = new InMemoryAnswerAttachmentRepository()
-        inMemoryRepository = new InMemoryAnswersRepository(inMemoryAnswerAttachment)
-        sut = new AnswerQuestionUseCase(inMemoryRepository)
-    })
+describe('Create an answer', () => {
 
-    it(' should be able to create an answer', async ()=>{
-        
-        const result = await sut.execute({
-            questionId:'1',
-            instructorId:'1',
-            content:'Nova Resposta',
-            attachmentsIds:['1', '2', '3']
-        })
-    
-        expect(result.isRight()).toBe(true)
-        expect(inMemoryRepository.items[0]).toEqual(result.value?.answer)
-        expect(inMemoryRepository.items[0].attachments.currentItems).toHaveLength(3)
-        expect(inMemoryRepository.items[0].attachments.currentItems).toEqual([
-            expect.objectContaining({attachmentId: new UniqueEntityId('1')}),
-            expect.objectContaining({attachmentId: new UniqueEntityId('2')}),
-            expect.objectContaining({attachmentId: new UniqueEntityId('3')})
-        ])
-    })
+	beforeEach(() => {
+		inMemoryAnswerAttachment = new InMemoryAnswerAttachmentRepository()
+		inMemoryRepository = new InMemoryAnswersRepository(inMemoryAnswerAttachment)
+		sut = new AnswerQuestionUseCase(inMemoryRepository)
+	})
+
+	it(' should be able to create an answer', async () => {
+
+		const result = await sut.execute({
+			questionId: '1',
+			instructorId: '1',
+			content: 'Nova Resposta',
+			attachmentsIds: ['1', '2', '3']
+		})
+
+		expect(result.isRight()).toBe(true)
+		expect(inMemoryRepository.items[0]).toEqual(result.value?.answer)
+		expect(inMemoryRepository.items[0].attachments.currentItems).toHaveLength(3)
+		expect(inMemoryRepository.items[0].attachments.currentItems).toEqual([
+			expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+			expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+			expect.objectContaining({ attachmentId: new UniqueEntityId('3') })
+		])
+	})
 })
